629a2505c41cf270d3fe594221ee6285eabd14e9
snadi043 <snadi043@uottawa.ca>
2025-03-17 11:39:21 -0400

    Refactored the code inorder to handle the connection of database efficiently and to reuse it effectively in the application.

--- util/database.js
+++ util/database.js
@@ -2,14 +2,30 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient; //Configuring MongoClient to connect to mongoDB.
 
+let _db;
+
 // Inorder to use the connection process as a reusable code, calling it with a method to export it and use in the application.
 const mongoConnect = (callback) => {
-  MongoClient.connect('mongodb+srv://NodeMongo:node-mongo-integration@node-mongo-integration.025ge.mongodb.net/?retryWrites=true&w=majority&appName=Node-Mongo-Integration')
+  MongoClient.connect('mongodb+srv://NodeMongo:node-mongo-integration@node-mongo-integration.025ge.mongodb.net/shop?retryWrites=true&w=majority&appName=Node-Mongo-Integration') //shop is the database name.
   .then(client => {
-    callback(client);
+    _db = client.db();
+    callback();
     console.log('Connected!');
   })
-  .catch(err => {console.log(err)});
+  .catch(err => {
+    console.log(err);
+    throw err;
+  });
 }
+
+// Storing the access to the database to efficently use the database connection.
+const getDb = () => {
+  if(_db){
+    return _db;
+  }
+  throw 'No database connection found';
+}
+
 // Exporting the connection process.
-module.exports = mongoConnect;
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
