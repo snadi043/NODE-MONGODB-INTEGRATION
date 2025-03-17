37ede91e0a5df830f4a2812dc623e16595420729
snadi043 <snadi043@uottawa.ca>
2025-03-17 10:15:27 -0400

    Installed MongoDB package using npm and integrated it with Mongo Atlas a cloud infrastrucutre provided by mongoDB to work with database related tasks.

--- util/database.js
+++ util/database.js
@@ -1,8 +1,15 @@
-const Sequelize = require('sequelize');
+// Importing the installed MongoDB pacakge to use in the application to integrate with the database.
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient; //Configuring MongoClient to connect to mongoDB.
 
-const sequelize = new Sequelize('node-development', 'root', 'swathiKumar@18', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
-
-module.exports = sequelize;
+// Inorder to use the connection process as a reusable code, calling it with a method to export it and use in the application.
+const mongoConnect = (callback) => {
+  MongoClient.connect('mongodb+srv://NodeMongo:node-mongo-integration@node-mongo-integration.025ge.mongodb.net/?retryWrites=true&w=majority&appName=Node-Mongo-Integration')
+  .then(client => {
+    callback(client);
+    console.log('Connected!');
+  })
+  .catch(err => {console.log(err)});
+}
+// Exporting the connection process.
+module.exports = mongoConnect;
