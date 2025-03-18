0c8f829531616cfb9925c80ef3143c929e9ecc5b
snadi043 <snadi043@uottawa.ca>
2025-03-18 19:31:08 -0400

    Created the methods fetchAll() and findById() to process the responses through mongoDb provided methods and cursors to handle the data efficiently.

--- models/product.js
+++ models/product.js
@@ -2,7 +2,12 @@
 
 // const sequelize = require('../util/database');
 
+// const mongodb = require('mongodb');
+
+const ObjectId = require('mongodb').ObjectId;
+
 const getDb = require('../util/database').getDb;
+
 
 class Product{
   constructor(title, price, imageUrl, description){
@@ -12,9 +17,10 @@ class Product{
     this.description = description;
   }
 
+  // Save() is the method to add the product to the database which is a post action.
   save(){
     const db = getDb();
-    return db.collection('products').insertOne(this)
+    return db.collection('products').insertOne(this) // insertOne is the mongoDB method to store the document/record into the database.
     .then(result => {
       console.log(result);
     })
@@ -22,7 +28,34 @@ class Product{
       console.log(err);
     });
   }
+
+  // fetchAll() is the static method to get all the products from the database which is the get action.
+  static fetchAll(){
+    const db = getDb();
+    return db.collection('products').find().toArray()
+    //find() is the mongodb method to fetch all the documents from the database. 
+    //toArray() is the cursor/method which is used to pass a limited amount of documents/records while fetching from the database insetead of bulk. 
+    .then(products => {
+      return products;
+    })
+    .catch(err => {console.log(err)});
+  }
+
+  // findById() is the static method to get a particular products from the database which is the get action based on the filter added to the collection.
+  static findById(prodId){
+    const db = getDb();
+    return db.collection('products')
+    .find({_id: new ObjectId(prodId)}) // find() can take an object which can have the filters to sort the data based on the conditions.
+    .next() // next() is the cursor/method to forward the functionality of the method in the process of handling the data.
+    .then(product => {
+      return product;
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
 }
+
 
 // const Product = sequelize.define('product', {
 //   id: {
