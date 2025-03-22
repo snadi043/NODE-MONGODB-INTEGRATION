b1e30d71b889005706717c0cf5700528e1b2a54d
snadi043 <snadi043@uottawa.ca>
2025-03-22 09:51:47 -0400

    Worked on the add to cart functionality in the application by implementing a new addToCart() method which expects a product as an parameter where a relation is made in between the user and the product through the cart to store the details of the product and the quantity which can be done be adding the cart and id as the properties to the user and using the updateOne() method in the users collection by updating just the product_id and the quantity by eliminating the redundancy of having product information in both products and users collections. Finally bringing back the routes in the shop to render the corresponding view of the response.

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.getUserById('67dc0bd3965d52b7a4cfe07f')
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
--- models/user.js
+++ models/user.js
@@ -5,9 +5,11 @@ const ObjectId = mongoDB.ObjectId;
 const getDb = require('../util/database').getDb;
 
 class Users{
-  constructor(name, email){
+  constructor(name, email, cart, id){
     this.name = name;
     this.email = email;
+    this.cart = cart; // {items: [{}]}
+    this._id = id;
   }
   save(){
     const db = getDb();
@@ -16,6 +18,13 @@ class Users{
   static getUserById(userId){
     const db = getDb();
     return db.collection('users').findOne({_id: new ObjectId(userId)});
+  }
+  // addToCart() is the method which depends upon the userId and makes a relation between user and the cart.
+  // Based on the userId, the cart gets the access to add the products.
+  addToCart(product){
+    const updatedCart = {items: [{ productId: new Object(product._id), quantity: 1}]};
+    const db = getDb();
+    return db.collection('users').updateOne({_id: new ObjectId(this._id)}, {$set: {cart: updatedCart}});
   }
 }
 
