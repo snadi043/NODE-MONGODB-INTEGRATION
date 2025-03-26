99c17f79c6bc3c090dcbcc57ea1357dfe0b37cd1
snadi043 <snadi043@uottawa.ca>
2025-03-26 19:33:25 -0400

    fetching the cart details from the collection using the populate method provided by mongoose.

--- controllers/shop.js
+++ controllers/shop.js
@@ -42,8 +42,11 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  req.user.getCart()
-    .then(products => {
+  req.user
+  .populate('cart.items.productId') // populate() is the mongoose provided method which gets all the fields from the collection that is specified in the method.
+  // .execPopulate()
+    .then(user => {
+      const products = user.cart.items;
       res.render('shop/cart', {
         path: '/cart',
         pageTitle: 'Your Cart',
