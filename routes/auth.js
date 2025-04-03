bfd3fc0311519f458a56c5e52d458caa7adf99ef
snadi043 <snadi043@uottawa.ca>
2025-04-03 10:32:15 -0400

    added the new auth routes to the app.js file to use it in the application.

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 
 const authController = require('../controllers/auth');
@@ -6,3 +8,5 @@ const router = express.Router();
 
 // GET -> HTTP GET method to handle the Login request from the navigation page in the app.
 router.get('/login', authController.getLogin);
+
+module.exports = router;
