09db5586877a704866f333eb5e4869e4fd3163d1
snadi043 <snadi043@uottawa.ca>
2025-04-04 09:11:36 -0400

    Installed express-session and connect-mongodb-session packages to implement the sessions configuration with the mongodb to store the sessions in the database collection which is really scalable and can mitigate memeory outages.

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const bodyParser = require('body-parser');
 
 const mongoose = require('mongoose');
 
+const session = require('express-session');
+const mongodbSessionStore = require('connect-mongodb-session')(session);
+
 //Importing the method to listen to the localhost and run the node application.
 // const mongoConnect = require('./util/database').mongoConnect;
 
@@ -21,8 +24,12 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
+const MONGODB_URI = 'mongodb+srv://NodeMongo:node-mongo-integration@node-mongo-integration.025ge.mongodb.net/shop?w=majority&appName=Node-Mongo-Integration';
+const store = new mongodbSessionStore({uri: MONGODB_URI, collection: 'sessions'});
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({secret: 'this is the most secret property to be set inorder to attain high security', saveUninitialized: false, resave: false, store: store}));
 
 app.use((req, res, next) => {
   User.findById('67e2bad2ede014c342373de4')
@@ -46,8 +53,7 @@ app.use(errorController.get404);
 
 // Connecting the mongoose package to the mongoDb to make use of ODM(Object Document Mapping) concept.
 // This bascially takes care of all the heavy lifting behind the scenes to create the collections & documents to store the data.
-mongoose.connect('mongodb+srv://NodeMongo:node-mongo-integration@node-mongo-integration.025ge.mongodb.net/shop?retryWrites=true&w=majority&appName=Node-Mongo-Integration')
-.then(result => {
+mongoose.connect(MONGODB_URI).then(result => {
   User.findOne().then(user => {
     if(!user){
       const user = new User({
--- controllers/auth.js
+++ controllers/auth.js
@@ -2,17 +2,19 @@
  
 exports.getLogin = (req, res, next) => {
     // const isLoggedIn = req.get('Cookie');
+    console.log(req.session.isLoggedIn);
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
-        isAuthenticated: req.isAuthenticated,
+        isAuthenticated: false,
     });
 }
 
 exports.postLogin = (req, res, next) => {
     // // While handling the action that is after login button is clicked, we plan to pass the isAuthenticated value through the request to display the reamining two routes conditionally.
-    req.isAuthenticated = true; 
-    res.setHeader('Set-Cookie', 'isLoggedIn=true');
+    // req.isAuthenticated = true; 
+    // res.setHeader('Set-Cookie', 'isLoggedIn=true');
+    req.session.isLoggedIn = true;
     res.redirect('/');
 }
 
@@ -21,3 +23,16 @@ exports.postLogin = (req, res, next) => {
 
 // Cookies -> Cookies are basically the system/procedure which helps to store the user information in the browser and use it while handling the HTTP request in between the client and the server.
 // Cookies are configured/enabled by using the setHeader() method in the request where it is required to store the value.
+
+// In the cookie as we have seen in the earlier case that the value is stored in the clinet side (browser) which can be manipulated by the users.
+// So, the alternative to avoid such a scenario is to use Sessions.
+
+// Sessions -> Sessions are basically defined as a feature to store the value of a user to handle all the HTTP requeests of the same user, which is stored in the memory/database so it is not lost or expired or manipulated like in cookies.
+// Sessions are basically stored in the backend of the application.
+// Sessions work with cookies where cookies store the session ID which is hashed which adds additional layer of security.
+
+// Though Sessions are useful and secured compared to cookies, the drawback with sessions is that they are stored in memory.
+// So, when dealing with tons of users in the production environment it might not be scalable and can lead to memory lekages.
+// So, the alternative to this is that, we use the procedure to store them (Sessions) in the database.
+// In this application, since mongoDB is integrated with the project, implementing the session storage in mongoDB is viable.
+// To implement this express-session package provides us with another store package which is "connect-mongodb-session".
